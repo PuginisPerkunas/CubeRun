df5a7fc01d727daad355991e876225b8560060cd
perkunasMacBook <yzyzz852@gmail.com>
2020-04-19 02:28:19 +0300

    new flow; created event system;

---
+++ Assets/Scripts/GameConstantsAndIds.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class GameConstantsAndIds : SingletonClassAbstractor<GameConstantsAndIds>
+{
+    private int enemyId = 0;
+
+    public int GetEnemyId(){
+        enemyId += 1;
+        return enemyId;
+    }
+}
